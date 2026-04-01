07135419f3bf50eb5f0432c3305dc82051bdf8f9
eneserat <erateeness@gmail.com>
2026-04-01 22:12:58 +0300

    Css Dosyası Düzenlendi.

---
+++ OnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
