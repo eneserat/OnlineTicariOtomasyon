6eda6bf69b68047386a1305246b7fa41a2596beb
eneserat <erateeness@gmail.com>
2026-04-02 12:02:56 +0300

    Ürün Ekleme Ve Listeleme Controller+ View TAMAM

---
+++ OnlineTicariOtomasyon/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Classes;
+
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class ProductController : Controller
+    {
+       Context c = new Context();
+        public ActionResult Index()
+        {
+            var urunler = c.Products.ToList();
+            return View(urunler);
+        }
+       
+        [HttpGet]
+        public ActionResult ProductAdd()
+        {
+          
+            ViewBag.Categories = new SelectList(c.Categories.ToList(), "CategoryID", "CategoryName");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ProductAdd(Product product)
+        {
+            c.Products.Add(product);
+            c.SaveChanges();
+
+            TempData["Success"] = "Ürün başarıyla eklendi!";
+            return RedirectToAction("Index");
+        }
+    }
+}
