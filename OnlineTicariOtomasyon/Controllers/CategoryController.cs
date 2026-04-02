dce51f392b8431879e3dec38c3ef145eaf07c063
eneserat <erateeness@gmail.com>
2026-04-02 09:54:00 +0300

    Category Class View+ Controller TAMAMLANDI

--- OnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ OnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OnlineTicariOtomasyon.Models.Classes;
 namespace OnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
-        
+        Context c = new Context();
+
         public ActionResult Index()
         {
+            var degerler = c.Categories.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult CategoryAdd()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult CategoryAdd(Category category)
+        {
+            c.Categories.Add(category);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+        public ActionResult CategoryRemove(int id )
+        {
+            var ctg = c.Categories.Find(id);
+            c.Categories.Remove(ctg);
+            c.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
