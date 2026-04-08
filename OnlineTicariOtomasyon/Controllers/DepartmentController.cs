442749a192fb62c1f9420f1a8644ea7703622777
eneserat <erateeness@gmail.com>
2026-04-08 21:11:07 +0300

    Departman View+Controller Tamamlandı

--- OnlineTicariOtomasyon/Controllers/DepartmentController.cs
+++ OnlineTicariOtomasyon/Controllers/DepartmentController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using OnlineTicariOtomasyon.Models.Classes;
 
@@ -9,21 +8,23 @@ namespace OnlineTicariOtomasyon.Controllers
     {
         Context c = new Context();
 
-        
+      
         public ActionResult Index()
         {
             var departments = c.Departments.ToList();
             return View(departments);
         }
 
-      
+       
         [HttpGet]
         public ActionResult DepartmentAdd()
         {
             return View();
         }
 
+        
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult DepartmentAdd(Department department)
         {
             if (ModelState.IsValid)
@@ -31,17 +32,17 @@ namespace OnlineTicariOtomasyon.Controllers
                 c.Departments.Add(department);
                 c.SaveChanges();
                 TempData["Success"] = "Departman başarıyla eklendi!";
-                return View(); 
+                return RedirectToAction("Index");
             }
             TempData["Error"] = "Departman eklenirken bir hata oluştu!";
             return View(department);
         }
 
-       
+        
         [HttpGet]
         public ActionResult DepartmentEdit(int? id)
         {
-            if (id == null)
+            if (!id.HasValue)
                 return RedirectToAction("Index");
 
             var dept = c.Departments.Find(id.Value);
@@ -52,6 +53,7 @@ namespace OnlineTicariOtomasyon.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult DepartmentEdit(Department department)
         {
             if (ModelState.IsValid)
@@ -67,16 +69,17 @@ namespace OnlineTicariOtomasyon.Controllers
                 existingDept.DepartmentImage = department.DepartmentImage;
                 c.SaveChanges();
 
-            
                 TempData["Success"] = "Departman başarıyla güncellendi!";
-                return RedirectToAction("Index");
+                return RedirectToAction("DepartmentEdit", new { id = department.DepartmentID });
             }
 
             TempData["Error"] = "Departman güncellenirken bir hata oluştu!";
             return View(department);
         }
 
+        
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult DepartmentDelete(int id)
         {
             var dept = c.Departments.Find(id);
@@ -89,10 +92,37 @@ namespace OnlineTicariOtomasyon.Controllers
             c.Departments.Remove(dept);
             c.SaveChanges();
 
-           
             TempData["Success"] = "Departman başarıyla silindi!";
             return RedirectToAction("Index");
         }
+
+        [HttpGet]
+        public ActionResult DepartmentDetails(int? id)
+        {
+            if (!id.HasValue)
+                return RedirectToAction("Index");
+
+            var dept = c.Departments
+                        .Where(d => d.DepartmentID == id.Value)
+                        .Select(d => new DepartmentDetailsVM
+                        {
+                            DepartmentID = d.DepartmentID,
+                            DepartmentName = d.DepartmentName,
+                            Employees = d.Employees
+                                         .Select(e => new EmployeeVM
+                                         {
+                                             Name = e.EmployeeName,
+                                             Surname = e.EmployeeSurname
+                                         }).ToList()
+                        })
+                        .FirstOrDefault();
+
+            if (dept == null)
+                return HttpNotFound();
+
+            return View(dept);
+        }
+
 
     }
 }
---
+++ OnlineTicariOtomasyon/Models/Classes/DepartmentDetailsVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineTicariOtomasyon.Models.Classes;    
+namespace OnlineTicariOtomasyon.Models.Classes
+{
+    public class DepartmentDetailsVM
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public List<string> EmployeeNames { get; set; }
+        public List<EmployeeVM> Employees { get; set; }
+
+    }
+    public class EmployeeVM
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
