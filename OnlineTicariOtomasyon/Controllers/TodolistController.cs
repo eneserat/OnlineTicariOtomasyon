f4f86720fd3764cdb6add4dff2421883ed6a8d35
eneserat <erateeness@gmail.com>
2026-04-22 00:29:17 +0300

    todo list.

--- OnlineTicariOtomasyon/Controllers/TodolistController.cs
+++ OnlineTicariOtomasyon/Controllers/TodolistController.cs
@@ -15,5 +15,45 @@ namespace OnlineTicariOtomasyon.Controllers
             var todolists = c.ToDoLists.ToList();
             return View(todolists); 
         }
+
+        [HttpPost]
+        public ActionResult Add(ToDoList p)
+        {
+            p.TodolistSituation = false;
+            c.ToDoLists.Add(p);
+            c.SaveChanges();
+
+            TempData["Message"] = "Görev Başarıyla Eklendi ✔";
+            TempData["Type"] = "success";
+
+            return RedirectToAction("Index");
+        }
+
+      
+        public ActionResult Complete(int id)
+        {
+            var task = c.ToDoLists.Find(id);
+
+            task.TodolistSituation = true;
+            c.SaveChanges();
+
+            TempData["Message"] = "Görev Tamamlandı ✔";
+            TempData["Type"] = "info";
+
+            return RedirectToAction("Index");
+        }
+
+      
+        public ActionResult Delete(int id)
+        {
+            var task = c.ToDoLists.Find(id);
+            c.ToDoLists.Remove(task);
+            c.SaveChanges();
+
+            TempData["Message"] = "Görev Başarıyla Silindi ❌";
+            TempData["Type"] = "danger";
+
+            return RedirectToAction("Index");
+        }
     }
 }
